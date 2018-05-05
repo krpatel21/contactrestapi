d5a489ac084cf74c14055d156d567b2369c9db26
Kaushal Patel <krpatel21@outlook.com>
2018-05-05 16:06:45 -0500

    Add functionality to sort by email.

--- src/main/java/com/example/contactrestfulapi/contact/ContactRepository.java
+++ src/main/java/com/example/contactrestfulapi/contact/ContactRepository.java
@@ -1,7 +1,13 @@
 package com.example.contactrestfulapi.contact;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-public interface ContactRepository extends CrudRepository<Contact, Long> {
 
+public interface ContactRepository extends PagingAndSortingRepository<Contact, Long> {
+    @RestResource(path = "email-is", rel = "email-is")
+    Page<Contact> findByEmailIs(@Param("email") String name, Pageable p);
 }
