d89919d11801933fb616db517ca0a89ce1bf62f1
Kaushal Patel <krpatel21@outlook.com>
2018-05-06 15:56:17 -0500

    Update contact repository.

--- src/main/java/com/example/contactrestfulapi/contact/ContactRepository.java
+++ src/main/java/com/example/contactrestfulapi/contact/ContactRepository.java
@@ -9,13 +9,25 @@ import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface ContactRepository extends PagingAndSortingRepository<Contact, Long> {
     @RestResource(path = "email-equals", rel = "emailEquals")
-    Page<Contact> findByEmailEquals(@Param("email") String name, Pageable p);
+    Contact findByEmailEquals(@Param("email") String name);
 
     @RestResource(path = "phone-equals", rel = "phoneEquals")
-    Page<Contact> findByPersonalPhoneEquals(@Param("phone") String phpme, Pageable p);
+    Contact findByPersonalPhoneEquals(@Param("phone") String phpme);
 
     @RestResource(path = "email-or-phone", rel = "emailOrPhoneEquals")
-    Page<Contact> findByEmailEqualsOrPersonalPhoneEquals(@Param("email") String email, @Param("phone") String phone, Pageable p);
+    Contact findByEmailEqualsOrPersonalPhoneEquals(@Param("email") String email, @Param("phone") String phone);
+
+    @RestResource(path = "city-and-state", rel = "cityAndStateEquals")
+    Page<Contact> findByCityAndStateEquals(@Param("city") String city, @Param("state") String state, Pageable p);
+
+    @RestResource(path = "city-or-state", rel = "cityOrStateEquals")
+    Page<Contact> findByCityOrStateEquals(@Param("city") String city, @Param("state") String state, Pageable p);
+
+    @RestResource(path = "city", rel = "city")
+    Page<Contact> findByCityEquals(@Param("city") String city, Pageable p);
+
+    @RestResource(path = "state", rel = "state")
+    Page<Contact> findByStateEquals(@Param("state") String city, Pageable p);
 
     @Override
     void delete(Contact contact);
